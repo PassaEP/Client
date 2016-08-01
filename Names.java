b66f030fc9ff1c01af995ce343c9cc0ac9393976
PassaEP <passapungchai@gmail.com>
2016-08-01 10:55:26 -0700

    added files

---
+++ Names.java
@@ -0,0 +1,10 @@
+public interface Names {
+
+   public static final String visitors [] = {
+      "ATurner", "BSimpson", "JMcVey", "CBall", "TEvans", "CHood", 
+      "MClifford", "MHealy", "NGrimshaw", "TFletcher", "HEdward",
+      "LHemmings", "AIrwin", "LWilliam", "NJames", "AWong"};
+
+   public static final String group [] = { "CMPS221", "CMPS222", "CMPS223", "CMPS342", "CMPS394" }; 
+    
+}
